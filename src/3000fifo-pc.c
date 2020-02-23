3c24bc2e4018804d8630da03abe38078116539a2
willfindlay <william@williamfindlay.com>
2020-02-22 20:08:28 -0500

    license

--- src/3000fifo-pc.c
+++ src/3000fifo-pc.c
@@ -1,7 +1,20 @@
-/* 3000pc.c */
-/* producer-consumer example using signals, processes and mmap */
-/* v1 Oct. 15, 2017 */
-/* Licenced under the GPLv3, copyright Anil Somayaji */
+/* 3000fifo-pc.c  Simple producer consumer with a fifo
+ * Original Version Copyright (C) 2017  Anil Somayaji
+ * Modified Version Copyright (C) 2020  William Findlay
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 /* You really shouldn't be incorporating parts of this in any other code,
    it is meant for teaching, not production */
 
